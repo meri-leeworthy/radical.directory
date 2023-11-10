0125689541d77ef64afcbb6e97b45fd1751d64bc
Meri Leeworthy <mlee0167@student.monash.edu>
2023-11-10 23:01:24 +1100

    remove more apollo

--- components/template/App.tsx
+++ components/template/App.tsx
@@ -1,31 +1,31 @@
-import Head from "next/head";
-import { Header } from "components/Header";
-import { useQuery } from "@apollo/client";
-import { AUTHENTICATED_USER, GET_USER_NAME } from "lib/apollo/queries";
+import Head from "next/head"
+import { Header } from "components/Header"
+// import { useQuery } from "@apollo/client";
+// import { AUTHENTICATED_USER, GET_USER_NAME } from "lib/apollo/queries";
 
 type Props = {
-  title: string;
-  children: JSX.Element | JSX.Element[];
-};
+  title: string
+  children: JSX.Element | JSX.Element[]
+}
 
 export const App = ({ children, title }: Props) => {
-  const { data } = useQuery(AUTHENTICATED_USER);
-  const { data: nameData } = useQuery(GET_USER_NAME, {
-    variables: {
-      id: data?.authenticatedUser?.id,
-    },
-  });
+  // const { data } = useQuery(AUTHENTICATED_USER);
+  // const { data: nameData } = useQuery(GET_USER_NAME, {
+  //   variables: {
+  //     id: data?.authenticatedUser?.id,
+  //   },
+  // });
 
   return (
     <div className="flex flex-col items-center w-screen h-screen">
       <Head>
         <title>{title}</title>
       </Head>
-      <Header
+      {/* <Header
         name={nameData?.user?.name || "stranger"}
         isLoggedIn={!!data?.authenticatedUser?.id}
-      />
+      /> */}
       {children}
     </div>
-  );
-};
+  )
+}
