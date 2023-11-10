f9f926c7cac943a9be89fe8dce23bf014919365c
Meri Leeworthy <mlee0167@student.monash.edu>
2023-11-10 23:07:56 +1100

    app component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,17 @@
 import "../styles/globals.css"
 import type { AppProps } from "next/app"
-import { ThemeProvider } from "next-themes"
-import { WithThemes } from "components/WithThemes"
-import * as Tooltip from "@radix-ui/react-tooltip"
+// import { ThemeProvider } from "next-themes"
+// import { WithThemes } from "components/WithThemes"
+// import * as Tooltip from "@radix-ui/react-tooltip"
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     // <ThemeProvider attribute="class" enableSystem={true}>
-    <Tooltip.TooltipProvider>
-      <WithThemes>
-        <Component {...pageProps} />
-      </WithThemes>
-    </Tooltip.TooltipProvider>
+    // <Tooltip.TooltipProvider>
+    //   <WithThemes>
+    <Component {...pageProps} />
+    //   </WithThemes>
+    // </Tooltip.TooltipProvider>
     // </ThemeProvider>
   )
 }
