171282522885dab3210905a71a4bfd9aca30e53e
Meri Leeworthy <mlee0167@student.monash.edu>
2023-11-10 23:20:53 +1100

    rename pages folder

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,12 +2,15 @@ import type { Config } from 'tailwindcss'
 
 const config: Config = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    // './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   mode: "jit",
-  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}",'./app/**/*.{js,ts,jsx,tsx,mdx}'],
+  purge: [
+    // "./pages/**/*.{js,ts,jsx,tsx}",
+   "./components/**/*.{js,ts,jsx,tsx}",
+   './app/**/*.{js,ts,jsx,tsx,mdx}'],
   darkMode: "class", // or 'media' or 'class'
   theme: {
     fontFamily: {
