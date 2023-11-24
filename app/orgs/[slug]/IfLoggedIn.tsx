7809820cf1434be6c6bbd67cebaefb6448d3cda5
Meri Leeworthy <mlee0167@student.monash.edu>
2023-11-25 00:20:45 +1100

    hydration error fix

--- app/orgs/[slug]/IfLoggedIn.tsx
+++ app/orgs/[slug]/IfLoggedIn.tsx
@@ -1,9 +1,16 @@
 "use client"
 
+import { useState, useEffect } from "react"
+
 export function IfLoggedIn({ children }: { children: React.ReactNode }) {
-  if (typeof window === "undefined") return
+  const [isClient, setIsClient] = useState(false)
+  useEffect(() => {
+    if (typeof window === "undefined") return
+    setIsClient(true)
+  }, [])
+
   const accessToken = localStorage.getItem("accessToken")
   const userId = localStorage.getItem("userId")
 
-  return <>{accessToken && userId && children}</>
+  return <>{isClient && accessToken && userId && children}</>
 }
