5ae90216c47c7a7361e34f4a6cd8829b8e9a7b80
Meri Leeworthy <mlee0167@student.monash.edu>
2023-11-29 07:49:23 +1100

    revalidate access token endpoint

--- app/api/revalidate/route.ts
+++ app/api/revalidate/route.ts
@@ -1,11 +1,17 @@
-import { revalidatePath } from "next/cache"
+import { revalidatePath, revalidateTag } from "next/cache"
 import { NextRequest, NextResponse } from "next/server"
 
 export async function GET(request: NextRequest) {
   const path = request.nextUrl.searchParams.get("path")
+  const tag = request.nextUrl.searchParams.get("tag")
 
   if (path) {
     revalidatePath(path)
+    return NextResponse.json({ revalidated: true, now: Date.now() })
+  }
+
+  if (tag) {
+    revalidateTag(tag)
     return NextResponse.json({ revalidated: true, now: Date.now() })
   }
 
--- lib/getServerAccessToken.ts
+++ lib/getServerAccessToken.ts
@@ -1,18 +1,28 @@
-const { RD_PUBLIC_USERNAME, RD_PUBLIC_PASSWORD, MATRIX_BASE_URL } = process.env
+const {
+  RD_PUBLIC_USERNAME,
+  RD_RELAY_USERNAME,
+  RD_PUBLIC_PASSWORD,
+  RD_RELAY_PASSWORD,
+  MATRIX_BASE_URL,
+} = process.env
 
-export async function getServerAccessToken() {
+export async function getServerAccessToken(type?: "public" | "relay") {
+  type = type || "public"
+  const username = type === "relay" ? RD_RELAY_USERNAME : RD_PUBLIC_USERNAME
+  const password = type === "relay" ? RD_RELAY_PASSWORD : RD_PUBLIC_PASSWORD
   const response = await fetch(`${MATRIX_BASE_URL}/_matrix/client/v3/login`, {
     method: "POST",
     body: JSON.stringify({
       type: "m.login.password",
       identifier: {
         type: "m.id.user",
-        user: RD_PUBLIC_USERNAME,
+        user: username,
       },
-      password: RD_PUBLIC_PASSWORD,
+      password: password,
     }),
     next: {
       revalidate: 3600 * 24,
+      tags: ["access_token"],
     },
   })
   const data = await response.json()
